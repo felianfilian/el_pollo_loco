61d70c169801d0a294ba33ab82451c2dbd121005
Dennis Schmalstieg <darianbellentor@outlook.de>
2023-07-15 20:29:56 +0200

    setup throwable constructor

--- js/throwable.class.js
+++ js/throwable.class.js
@@ -1,3 +1,9 @@
 class Throwable extends Movable {
-  constructor() {}
+  constructor(x, y) {
+    super().loadImage("img/6_salsa_bottle/salsa_bottle.png");
+    this.x = x;
+    this.y = y;
+    this.height = 60;
+    this.width = 50;
+  }
 }
